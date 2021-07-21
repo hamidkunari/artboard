29245575cf470c247c879b0e832c598a2dc82c02
Alan Yeong <alan_yeong@live.com>
2021-07-22 00:30:47 +0800

    Added in Federal Territories as well in State.

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -108,12 +108,14 @@
 										<option value="Kedah">Kedah</option>
 										<option value="Kelantan">Kelantan</option>
 										<option value="Kuala Lumpur">Kuala Lumpur</option>
+										<option value="Labuan">Labuan</option>
 										<option value="Melaka">Melaka</option>
 										<option value="Negeri Sembilan">Negeri Sembilan</option>
 										<option value="Pahang">Pahang</option>
 										<option value="Penang">Penang</option>
 										<option value="Perak">Perak</option>
 										<option value="Perlis">Perlis</option>
+										<option value="Putrajaya">Putrajaya</option>
 										<option value="Terengganu">Terengganu</option>
 										<option value="Sabah">Sabah</option>
 										<option value="Sarawak">Sarawak</option>
